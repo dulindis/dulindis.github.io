ec962f637ccb0d1469ed54f1c6623cd303f113f3
POkulska <paulina.okulska@gmail.com>
2023-04-04 12:48:02 +0200

    adjusted drawer responsive styles

--- src/components/Navigation/styles.js
+++ src/components/Navigation/styles.js
@@ -6,24 +6,31 @@ import ListItemIcon from "@mui/material/ListItemIcon";
 export const StyledButton = styled(Button)(({ theme, state, anchor }) => ({
   position: "absolute",
   left: state[anchor] ? "250px" : "0px",
-  top: "50%",
-  [theme.breakpoints.only("xs")]: {
-    top: "30px",
-  },
+  top: "80px",
+  left: "4rem",
+  // top: "50%",
+  // [theme.breakpoints.only("xs")]: {
+  //   top: "3%",
+  // },
   transform: "translate(0, -50%)",
   zIndex: 2,
   transition: "ease 0.5",
-  fontSize: "1.5rem",
+  fontSize: "2rem",
   color: "primary.main",
   backgroundColor: "transparent",
   [theme.breakpoints.only("xs")]: {
     backgroundColor: "rgba(238, 236, 236,0.7)",
+    top: "40px",
+    left: "10px",
   },
 }));
 
 export const StyledDrawer = styled(Drawer)(({ theme }) => ({
   ".MuiDrawer-root": {
     width: "270px",
+    [theme.breakpoints.only("xs")]: {
+      width: "220px",
+    },
     zIndex: "2",
     flexShrink: 0,
   },
@@ -33,6 +40,9 @@ export const StyledDrawer = styled(Drawer)(({ theme }) => ({
     alignContent: "center",
     justifyContent: "center",
     width: "270px",
+    [theme.breakpoints.only("xs")]: {
+      width: "220px",
+    },
     boxSizing: "border-box",
     backgroundColor: "#fff",
     color: "#343434",
@@ -43,6 +53,6 @@ export const StyledListItemIcon = styled(ListItemIcon)(({ theme }) => ({
   color: "primary.main",
   marginLeft: "50px",
   [theme.breakpoints.only("xs")]: {
-    backgroundColor: "35px",
+    marginLeft: "35px",
   },
 }));
